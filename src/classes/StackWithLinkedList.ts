2ffe441487b14c44a8ceb337981f16fbddbc89c5
Emerson Paiva <emersonaraujopaiva@gmail.com>
2022-08-05 21:46:54 -0300

    feat: add StackWithLinkedList

---
+++ src/classes/StackWithLinkedList.ts
@@ -0,0 +1,50 @@
+import { Node } from './LinkedList';
+
+export class StackWithLinkedList<T> {
+  private length = 0;
+  private top: Node<T> = null;
+
+  peek() {
+    return this.top;
+  }
+
+  push(element: T) {
+    const newNode = new Node(element);
+
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      const holdingPointer = this.top;
+
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return null;
+    }
+
+    const holdingPointer = this.top;
+
+    this.top = this.top.next;
+    this.length--;
+
+    return holdingPointer;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const myStack = new StackWithLinkedList();
+
+myStack.push('Google');
+myStack.push('Udemy');
+myStack.push('Discord');
+console.log(myStack.pop());
